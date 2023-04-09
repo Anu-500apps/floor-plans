c90958dcb70f2bb25a9207a052486e52ebbed491
Anu-500apps <anuradha.taddi@500apps.com>
2023-04-09 18:45:23 +0530

    Builders

---
+++ components/builder/Add.vue
@@ -0,0 +1,177 @@
+<template>
+  <!-- start addform sidebar -->
+  <TransitionRoot as="template" :show="open">
+    <Dialog as="div" class="relative z-10" @close="open = false">
+      <div class="fixed inset-0 overflow-hidden">
+        <div class="absolute inset-0 overflow-hidden">
+          <div
+            class="pointer-events-none fixed inset-y-0 right-0 flex max-w-full pl-10"
+          >
+            <TransitionChild
+              as="template"
+              enter="transform transition ease-in-out duration-500 sm:duration-700"
+              enter-from="translate-x-full"
+              enter-to="translate-x-0"
+              leave="transform transition ease-in-out duration-500 sm:duration-700"
+              leave-from="translate-x-0"
+              leave-to="translate-x-full"
+            >
+              <DialogPanel class="pointer-events-auto w-screen max-w-md">
+                <div
+                  class="flex h-full flex-col overflow-y-scroll bg-white py-6 shadow-xl"
+                >
+                  <div class="px-4 sm:px-6">
+                    <div class="flex items-start justify-between">
+                      <DialogTitle
+                        class="text-base font-semibold leading-6 text-gray-900"
+                        >Add Builder</DialogTitle
+                      >
+                      <div class="ml-3 flex h-7 items-center">
+                        <button
+                          class="rounded-md bg-white text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                          @click="open = false"
+                        >
+                          <span class="sr-only">Close panel</span>
+                          <XMarkIcon class="h-6 w-6" aria-hidden="true" />
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                  <!-- Start Builder Form -->
+                  <form>
+                    <div
+                      class="relative mt-6 flex-1 px-4 sm:px-6 border ml-2 mr-2"
+                    >
+                      <div class="mt-2">
+                        <label
+                          for="name"
+                          class="block text-sm font-medium leading-6 text-gray-900"
+                          > Name</label
+                        >
+                        <div class="mt-2">
+                          <input
+                            type="text"
+                            id="name"
+                            v-model="formData.name"
+                            class="pl-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                            placeholder="Enter Name"
+                          />
+                        </div>
+                      </div>
+                      <div class="mt-2">
+                        <label
+                          for="number"
+                          class="block text-sm font-medium leading-6 text-gray-900"
+                          >Phone Number</label
+                        >
+                        <div class="mt-2">
+                          <input
+                            type="text"
+                            id="number"
+                            v-model="formData.phone_number"
+                            class="pl-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                            placeholder="Enter Phone Number"
+                          />
+                        </div>
+                      </div>
+
+                      <div class="mt-2">
+                        <label
+                          for="email"
+                          class="block text-sm font-medium leading-6 text-gray-900"
+                          >Email</label
+                        >
+                        <div class="mt-2">
+                          <input
+                            type="text"
+                            id="email"
+                            v-model="formData.email"
+                            class="pl-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                            placeholder="Enter Email"
+                          />
+                        </div>
+                      </div>
+
+                      <div class="mt-2">
+                        <label
+                          for="year"
+                          class="block text-sm font-medium leading-6 text-gray-900"
+                          >Established Year
+                        </label>
+                        <div class="mt-2">
+                          <input
+                            type="text"
+                            id="year"
+                            v-model="formData.established_year"
+                            class="pl-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                            placeholder="Enter Established Year "
+                          />
+                        </div>
+                      </div>
+                      <div class="mt-2">
+                        <div class="mt-2 mb-2 ml-[15.25rem] flex">
+                          <div>
+                            <button
+                              @click="open = false"
+                              class="mr-1 rounded bg-white px-2 py-1 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+                            >
+                              Cancel
+                            </button>
+                          </div>
+                          <div>
+                          
+                            <button
+                              class="rounded bg-indigo-600 px-2 py-1 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                              @click="submitBuilderDetails(), (open = false)"
+                            >
+                              Submit
+                            </button>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </form>
+                  <!-- End Builder Form -->
+                </div>
+              </DialogPanel>
+            </TransitionChild>
+          </div>
+        </div>
+      </div>
+    </Dialog>
+  </TransitionRoot>
+  <!-- end addform sidebar -->
+</template>
+<script setup lang="ts">
+// import Statements
+import {
+  Dialog,
+  DialogPanel,
+  DialogTitle,
+  TransitionChild,
+  TransitionRoot,
+} from "@headlessui/vue";
+import { XMarkIcon, PencilSquareIcon } from "@heroicons/vue/24/outline";
+import { ref, defineProps, defineEmits } from "vue";
+// Define Props
+const props = defineProps({
+  open: Boolean,
+});
+// Define Emits
+const emits = defineEmits(["submit-form-data"]);
+const formData = ref({
+  name: "",
+  email: "",
+  phone_number: "",
+  established_year: "",
+  logo: "logo",
+  tag_line: "string",
+  reg_details: "string",
+  reg_rera_url: "string",
+});
+
+// To submit form when user will click on submit button
+const submitBuilderDetails = () => {
+   emits("submit-form-data", formData.value);
+};
+</script>
---
+++ components/builder/List.vue
@@ -0,0 +1,139 @@
+<template>
+  <div class="px-4 sm:px-6 lg:px-8">
+    <div class="sm:flex sm:items-center font-extrabold font-mono">
+      <div class="sm:flex-auto">
+        <h1 class="text-base font-semibold leading-6 text-gray-900">
+          Builders
+        </h1>
+        <p class="mt-2 text-sm text-gray-700">A list of all the Builders</p>
+      </div>
+      <div class="mt-4 sm:ml-16 sm:mt-0 sm:flex-none ">
+        <button
+          type="button"
+          class="block rounded-md bg-gray-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-gray-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          @click="emits('open-sidebar-form')"
+        >
+          Add Builder
+        </button>
+      </div>
+    </div>
+    <div class="mt-8 flow-root">
+      <div class="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+        <div class="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
+          <div
+            class="overflow-hidden shadow ring-1 ring-black ring-opacity-5 sm:rounded-lg"
+          >
+            <!-- builder table start -->
+            <table class="min-w-full divide-y divide-gray-300 font-extrabold font-mono">
+              <thead class="bg-gray-500">
+                <tr>
+                  <th
+                    scope="col"
+                    class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6"
+                  >
+                    Name
+                  </th>
+                  <th
+                    scope="col"
+                    class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+                  >
+                    Phone Numbers
+                  </th>
+                  <th
+                    scope="col"
+                    class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+                  >
+                    Email
+                  </th>
+                  <th
+                    scope="col"
+                    class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+                  >
+                    Established Year
+                  </th>
+                  <th
+                    scope="col"
+                    class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+                  >
+                    logo
+                  </th>
+                  <th
+                    scope="col"
+                    class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+                  >
+                    Tag Line
+                  </th>
+                  <th scope="col" class="relative py-3.5 pl-3 pr-4 sm:pr-6">
+                    <span class="sr-only">Edit</span>
+                  </th>
+                </tr>
+              </thead>
+              <tbody class="divide-y divide-gray-200 bg-white">
+                <tr v-for="(builder, index) in buildersDetails" :key="index">
+                  <td
+                    class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6"
+                  >
+                    {{ builder.name }}
+                  </td>
+                  <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                    {{ builder.phone_number }}
+                  </td>
+                  <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                    {{ builder.email }}
+                  </td>
+                  <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                    {{ builder.established_year }}
+                  </td>
+                  <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                    {{ builder.logo }}
+                  </td>
+                  <td
+                    class="whitespace-nowrap px-3 py-4 text-sm text-gray-500"
+                  >
+                    {{ builder.tag_line }}
+                  </td>
+                  <td
+                    class="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-6"
+                  >
+                    <div class="flex">
+                      <PencilSquareIcon
+                        class="h-6 w-6 text-blue-700"
+                        @click="emits('edit-row', builder)"
+                      ></PencilSquareIcon>
+                      <TrashIcon
+                        class="h-6 w-6 text-red-700"
+                        @click="emits('delete-row', builder,index)"
+                      ></TrashIcon>
+                    </div>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+            <!-- builder table start -->
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+// Define Import Statements
+import { PencilSquareIcon, TrashIcon } from "@heroicons/vue/24/outline";
+import { ref, defineProps, defineEmits } from "vue";
+import {
+  Dialog,
+  DialogPanel,
+  DialogTitle,
+  TransitionChild,
+  TransitionRoot,
+} from "@headlessui/vue";
+
+const open = ref(false);
+// Define Props
+const props = defineProps({
+  buildersDetails: Array,
+});
+// Define Emits
+const emits = defineEmits(["open-sidebar-form","delete-row","edit-row",]);
+</script>
